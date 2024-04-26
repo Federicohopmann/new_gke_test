2fc73b50978b9987a3329083a7d7297e69dd4768
federicoerroi <federico.erroi@hopmann.net>
2024-04-26 17:12:19 +0200

    first commit

---
+++ app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+#Create a Flask application
+app = Flask(__name__)
+
+#Define a route for the root URL
+@app.route('/')
+def hello_world():
+    return "Ciao Federico"
+
+#Run the Flask application if this file is executed directly
+if __name__ == '__main__':
+    app.run(debug=True, port=8000, host='0.0.0.0')
+
